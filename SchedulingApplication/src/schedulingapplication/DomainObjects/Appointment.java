8a264a231bd10b5fb7b50e60ee73f80c9776dabe
Mark Pilkington <Mark.Pilkington@agdata.com>
2018-07-31 20:31:54 -0400

    changes 2 for today

--- SchedulingApplication/src/schedulingapplication/DomainObjects/Appointment.java
+++ SchedulingApplication/src/schedulingapplication/DomainObjects/Appointment.java
@@ -1,8 +1,78 @@
 
 package schedulingapplication.DomainObjects;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 
 public class Appointment {
+
+    private final SimpleIntegerProperty appointmentId;
+    private final SimpleStringProperty title;
+    private final SimpleStringProperty description;
+    private final SimpleStringProperty location;
+    private final SimpleStringProperty contact;
+    private final SimpleStringProperty URL;
+    
+    public Appointment(int appointmentId, String title, String description, String location,
+        String contact, String URL){
+        this.appointmentId = new SimpleIntegerProperty(appointmentId);
+        this.title = new SimpleStringProperty(title);
+        this.description = new SimpleStringProperty(description);
+        this.location = new SimpleStringProperty(location);
+        this.contact = new SimpleStringProperty(contact);
+        this.URL = new SimpleStringProperty(URL);
+    }
+
+    public int getAppointmentId() {
+        return appointmentId.get();
+    }
+
+    public void setCustomerId(int customerId) {
+        this.addressId.set(addressId);
+    }
+
+    public IntegerProperty customerId() {
+        return this.customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName.get();
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName.set(customerName);
+    }
+
+    public StringProperty customerNameProperty() {
+        return this.customerName;
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+
+    public StringProperty addressProperty() {
+        return this.address;
+    }
+
+    public String getPhone() {
+        return phone.get();
+    }
+
+    public void setPhone(String phone) {
+        this.phone.set(phone);
+    }
+
+    public StringProperty phoneProperty() {
+        return this.phone;
+    }
+    
     
 }
