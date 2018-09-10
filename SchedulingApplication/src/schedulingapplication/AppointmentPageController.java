704c1a25bf9391dabd163bdfad6f617616ee22f5
clplya <clplya@gmail.com>
2018-09-10 19:40:04 -0400

    9/10 commit
    changes to appointments

--- SchedulingApplication/src/schedulingapplication/AppointmentPageController.java
+++ SchedulingApplication/src/schedulingapplication/AppointmentPageController.java
@@ -1,11 +1,13 @@
 package schedulingapplication;
 
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 import java.time.chrono.ChronoLocalDate;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import schedulingapplication.Dao.DBAppointmentDao;
@@ -33,17 +35,18 @@ public class AppointmentPageController {
     @FXML
     private TextField apptContactField;
     @FXML
-    private javafx.scene.control.ComboBox selectedAppointmentComboBox;
+    private ComboBox<String> selectedAppointmentComboBox = new ComboBox<String>();
     @FXML
     private ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
 
-    public void initialize(Customer customer) {
+    public void initialize(Customer customer) throws InvocationTargetException {
+
         selectedCustomer = customer;
         appointmentList.addAll(dbAppointment.getAppointment(customer.getCustomerId()));
 
         for (int i = 0; i < appointmentList.size(); i++) {
-            //selectedAppointmentComboBox.setItems(appointmentList.get(i).getTitle());
-            selectedAppointmentComboBox.itemsProperty().setValue(i);
+            selectedAppointmentComboBox.setItems(appointmentList);
+            //selectedAppointmentComboBox.itemsProperty().setValue(i);
         }
     }
 
