ceb468c790df948d0bcc04a24a2fdddc0f4eb6d5
Mark Pilkington <Mark.Pilkington@agdata.com>
2018-11-04 12:57:30 -0500

    11-4
    checkin

--- SchedulingApplication/src/schedulingapplication/ConsultantScheduleReportController.java
+++ SchedulingApplication/src/schedulingapplication/ConsultantScheduleReportController.java
@@ -2,26 +2,60 @@ package schedulingapplication;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import schedulingapplication.Dao.DBReportsDao;
+
 
 public class ConsultantScheduleReportController implements Initializable {
 
     @FXML
     public TableView apptTable;
     @FXML
+    public TableColumn customerColumn;
+    @FXML
+    public TableColumn apptStartTimeColumn;
+    @FXML
+    public TableColumn apptStopTimeColumn;
+    @FXML
+    public TableColumn LocationColumn;
+    @FXML
     public ComboBox consultantComboBox;
-    
+    @FXML
+    ObservableList consultantList = FXCollections.observableArrayList();
+    DBReportsDao dbReports;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        customerColumn.setCellValueFactory(new PropertyValueFactory<>("customer"));
+        apptStartTimeColumn.setCellValueFactory(new PropertyValueFactory<>("aptStartTime"));
+        apptStartTimeColumn.setCellValueFactory(new PropertyValueFactory<>("aptStopTime"));
+                LocationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        System.out.println("Setting & Adding all Customers");
+
+        apptTable.setItems(null);
+        apptTable.setItems(loadAllConsultants());
+
+        consultantList = apptTable.getItems();
     }
-    
+
     @FXML
-    public void comboBoxEventHandler(){
-        
+    public ObservableList loadAllConsultants() {
+        consultantList.addAll(dbReports.selectAllConsultantNames());
+
+        return consultantList;
+    }
+
+    @FXML
+    public void comboBoxEventHandler() {
+
     }
 
 }
--- SchedulingApplication/src/schedulingapplication/DAO/DBReportsDao.java
+++ SchedulingApplication/src/schedulingapplication/DAO/DBReportsDao.java
@@ -10,6 +10,7 @@ import java.util.Date;
 public class DBReportsDao implements IReportsDao {
 
     private ArrayList<String> monthsOfAppts;
+    private ArrayList<String> consultants;
 
     public DBReportsDao() {
     }
@@ -52,52 +53,26 @@ public class DBReportsDao implements IReportsDao {
         return monthsOfAppts;
     }
 
-    public String countNumberOfMonths(int numberOfMonths) {
-        String monthString;
+    public ArrayList selectAllConsultantNames() {
+        Statement stmt = null;
 
-        switch (numberOfMonths) {
-            case 1:
-                monthString = "January";
-                break;
-            case 2:
-                monthString = "February";
-                break;
-            case 3:
-                monthString = "March";
-                break;
-            case 4:
-                monthString = "April";
-                break;
-            case 5:
-                monthString = "May";
-                break;
-            case 6:
-                monthString = "June";
-                break;
-            case 7:
-                monthString = "July";
-                break;
-            case 8:
-                monthString = "August";
-                break;
-            case 9:
-                monthString = "September";
-                break;
-            case 10:
-                monthString = "October";
-                break;
-            case 11:
-                monthString = "November";
-                break;
-            case 12:
-                monthString = "December";
-                break;
-            default:
-                monthString = "Invalid Month";
-                break;
+        try {
+            Connection conn = schedulingapplication.Dao.DataSource.getConnection();
+
+            stmt = conn.createStatement();
+            String sql = "select userName from user";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String Name = rs.getString(1);
+
+                consultants.add(Name);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+
         }
-
-        return monthString;
+        return consultants;
     }
 
     @Override
